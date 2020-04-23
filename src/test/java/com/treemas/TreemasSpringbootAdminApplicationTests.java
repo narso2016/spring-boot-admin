aa28bab737d606872a290de2b6986d175bdab30f
Munarso <narsommunarso@gmail.com>
2020-04-23 09:50:34 +0700

    first commit

---
+++ src/test/java/com/treemas/TreemasSpringbootAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.treemas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreemasSpringbootAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
